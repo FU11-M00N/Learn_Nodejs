01d616da8f3dba7ed7b1acdaaa405184b5c89855
문호준 <moonhojoon@munhojun-ui-MacBookPro.local>
2023-01-02 08:13:44 +0900

    feat: nickname 수정

--- 9_node_birdSNS/controllers/user.js
+++ 9_node_birdSNS/controllers/user.js
@@ -4,7 +4,7 @@ exports.follow = async (req, res, next) => {
    //req.user.id, req.params.id
    try {
       const user = await User.findOne({ where: { id: req.user.id } });
-      console.log('여기까지오냐양야야앙아아앙');
+
       if (user) {
          await user.addFollowing(parseInt(req.params.id, 10));
          res.send('success');
@@ -21,9 +21,31 @@ exports.unfollow = async (req, res, next) => {
    //req.user.id, req.params.id
    try {
       const user = await User.findOne({ where: { id: req.user.id } });
-      console.log('여기까지옴?asdadssa');
+
       if (user) {
          await user.removeFollowing(parseInt(req.params.id, 10));
+         res.send('success');
+      } else {
+         res.status(404).send('User not found');
+      }
+   } catch (error) {
+      console.error(error);
+      next(error);
+   }
+};
+
+exports.update_nick = async (req, res, next) => {
+   try {
+      const user = await User.findOne({ where: { id: req.user.id } });
+      if (user) {
+         User.update(
+            {
+               nick: req.body.nick,
+            },
+            {
+               where: { id: req.user.id },
+            },
+         );
          res.send('success');
       } else {
          res.status(404).send('User not found');
--- 9_node_birdSNS/routes/user.js
+++ 9_node_birdSNS/routes/user.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { isLoggedIn } = require('../middlewares');
-const { follow, unfollow } = require('../controllers/user');
+const { follow, unfollow, update_nick } = require('../controllers/user');
 
 router.post('/:id/follow', isLoggedIn, follow);
 router.delete('/:id/follow', isLoggedIn, unfollow);
+router.patch('/', isLoggedIn, update_nick);
 
 module.exports = router;
